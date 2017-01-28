f231e6e06a663dd77c62296561e264b0ef43ce79
mikkokut <mikko93@gmail.com>
2017-01-28 21:49:36 +0200

    handle not found items correctly

--- server.js
+++ server.js
@@ -52,105 +52,65 @@ app.get('/images/:id/file', function(req, res) {
 	console.log('find image ' + id);
 
 	// Get image
-	Image.findById(id).then(
-		image => {
+	Image.findById(id, (error, image) => {
+		if (error || !image) {
+			return res.status(404).send('Image not found');
+		}
 
-			//
-			// Resize
-			// 
+		//
+		// Resize
+		// 
 
-			const sizeKey = `s3_${size}_url`;
+		const sizeKey = `s3_${size}_url`;
 
-			// Check if size exists
-			if (!image[sizeKey]) {
+		// Check if size exists
+		if (!image[sizeKey]) {
 
-				// If we are fetching thumbnail and we already have the 
-				// large file use it instead to save bandwith
-				var url = image.image_url;
+			// If we are fetching thumbnail and we already have the 
+			// large file use it instead to save bandwith
+			var url = image.image_url;
+			console.log(url);
+			request.get({url, encoding: null}, function (_, _, body) {
 
-				request.get({url, encoding: null}, function (_, _, body) {
+				var func = null;
+				if (size === 'thumbnail') {
+					func = sharp(body)
+						.resize(null, 700)
+						.max()
+				}
+				else if(size === 'large') {
+					func = sharp(body)
+						.resize(1800, null)
+						.max()
+				}
 
-					var func = null;
-					if (size === 'thumbnail') {
-						func = sharp(body)
-							.resize(null, 700)
-							.max()
+				func.toBuffer((err, buffer, info) => {
+					if (err) {
+						console.log(err);
+						return res.status(500).send('Resize failed.');
 					}
-					else if(size === 'large') {
-						func = sharp(body)
-							.resize(1800, null)
-							.max()
-					}
 
-					func.toBuffer((err, buffer, info) => {
+					const path = `images/${id}_${size}.jpg`;
+					s3.putObject({
+						Bucket: process.env.S3_BUCKET,
+						Key: path,
+						ACL: 'public-read',
+						ContentDisposition: 'inline',
+						ContentType: 'image/jpg',
+						Body: buffer
+					}, (err, data) => {
 						if (err) {
 							console.log(err);
-							return res.status(500).send('Resize failed.');
+							return res.status(500).send('S3 upload failed.');
 						}
 
-						const path = `images/${id}_${size}.jpg`;
-						s3.putObject({
-							Bucket: process.env.S3_BUCKET,
-							Key: path,
-							ACL: 'public-read',
-							ContentDisposition: 'inline',
-							ContentType: 'image/jpg',
-							Body: buffer
-						}, (err, data) => {
-							if (err) {
-								console.log(err);
-								return res.status(500).send('S3 upload failed.');
-							}
-
-							
-							// Save the optimized url
-					        image[sizeKey] = 'https://images.rintamalla.fi/' + path;
-					        console.log(image);
-					        image.save().then(
-					        	image => {
-					        		
-					        		// Redirect to optimized url
-					        		res.redirect(301, image[sizeKey]);
-
-					        	}, error => {
-						        	console.log(error);
-						        	res.status(500).send('Image saving failed');
-					        	}
-					        );
-						});
-					});
-				});
-
-				
-				return;
-
-
-
-				// 
-				// Pass to kraken. Disabled for now.
-				// 
-				
-				const params = {
-				    url: url,
-				    lossy: true,
-				    resize: resizes[size],
-				    s3_store: {
-				        key: process.env.AWS_ACCESS_KEY_ID,
-				        secret: process.env.AWS_SECRET_ACCESS_KEY,
-				        bucket: process.env.S3_BUCKET,
-				        region: process.env.S3_REGION,
-				        path: `images/${id}_${size}`
-				    },
-				    wait: true
-				};
-				kraken.url(params, function(status) {
-				    if (status.success) {
-				        
-				        // Save the optimized url
-				        image[sizeKey] = status.kraked_url;
+						
+						// Save the optimized url
+				        image[sizeKey] = 'https://images.rintamalla.fi/' + path;
+				        console.log(image);
 				        image.save().then(
 				        	image => {
-
+				        		
 				        		// Redirect to optimized url
 				        		res.redirect(301, image[sizeKey]);
 
@@ -159,25 +119,63 @@ app.get('/images/:id/file', function(req, res) {
 					        	res.status(500).send('Image saving failed');
 				        	}
 				        );
-
-				    } else {
-				    	console.log(status);
-				    	res.status(500).send('Image resize failed. Error: ' + status.message);
-				    }
+					});
 				});
+			});
 
-			}
-			else {
+			
+			return;
 
-				// Redirect to optimized url
-				res.redirect(301, image[sizeKey]);
 
-			}
-		},
-		error => {
-			res.status(404).send('Image not found');
+
+			// 
+			// Pass to kraken. Disabled for now.
+			// 
+			
+			const params = {
+			    url: url,
+			    lossy: true,
+			    resize: resizes[size],
+			    s3_store: {
+			        key: process.env.AWS_ACCESS_KEY_ID,
+			        secret: process.env.AWS_SECRET_ACCESS_KEY,
+			        bucket: process.env.S3_BUCKET,
+			        region: process.env.S3_REGION,
+			        path: `images/${id}_${size}`
+			    },
+			    wait: true
+			};
+			kraken.url(params, function(status) {
+			    if (status.success) {
+			        
+			        // Save the optimized url
+			        image[sizeKey] = status.kraked_url;
+			        image.save().then(
+			        	image => {
+
+			        		// Redirect to optimized url
+			        		res.redirect(301, image[sizeKey]);
+
+			        	}, error => {
+				        	console.log(error);
+				        	res.status(500).send('Image saving failed');
+			        	}
+			        );
+
+			    } else {
+			    	console.log(status);
+			    	res.status(500).send('Image resize failed. Error: ' + status.message);
+			    }
+			});
+
 		}
-	);
+		else {
+
+			// Redirect to optimized url
+			res.redirect(301, image[sizeKey]);
+
+		}
+	});
 
 });
 
